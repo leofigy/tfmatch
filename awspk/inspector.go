3ad5d6dd735e57ac68c4f44850c117e71ab3f39e
leofigy <angel.jovany.figueroa@gmail.com>
2019-06-02 17:11:39 -0500

    quick test with interface

--- awspk/inspector.go
+++ awspk/inspector.go
@@ -2,6 +2,7 @@ package awspk
 
 import (
 	"fmt"
+	"reflect"
 
 	pkill "github.com/leofigy/aws_resources"
 )
@@ -9,8 +10,22 @@ import (
 func InspectResources(stack pkill.FactoryData) {
 	info, err := stack.CallAWS()
 	if err != nil {
-		fmt.Printf("Following errors found\n%s\n", err)
+		fmt.Printf("[CALLING ERRORS]\n%s\n", err)
 	}
-	fmt.Println(info)
+	for t := range info {
+		switch info[t].Kind() {
+		case reflect.Slice:
+			getItems(info[t])
+		default:
+			fmt.Println("Unexpected", info[t].Kind())
+		}
+	}
+}
 
+func getItems(list reflect.Value) {
+	if list.Len() > 0 {
+		for i := 0; i < list.Len(); i++ {
+			fmt.Printf("%v\n", list.Index(i).Interface())
+		}
+	}
 }
